25bd6ba527d6b9195920e930de7af3afb672cd8c
Theon ThinhTPT <thinhtpt.dev@gmail.com>
2023-03-07 16:46:33 +0700

    [Theon][0307] feat: add state chaging methods to order

---
+++ order-service/order-domain/order-domain-core/src/main/java/dev/thinhtpt/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package dev.thinhtpt.order.service.domain.entity;
+
+import dev.thinhtpt.domain.entity.AggregateRoot;
+import dev.thinhtpt.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
